d8d37b5df3490822829b3eedcfdc940263e2c876
Timur Sabirov <roboxtim@gmail.com>
2025-11-02 14:33:03 +0200

    Fix: remove unused Link import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { AppStoreButton } from '@/components/landing/AppStoreButton';
 import { FeatureCard } from '@/components/landing/FeatureCard';
