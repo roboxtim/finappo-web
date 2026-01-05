569c42bfd912f7df67ce461cab3bc85bcb9c9921
Timur Sabirov <roboxtim@gmail.com>
2026-01-05 15:25:01 +0200

    Add Google AdSense verification code to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1053605285835650"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
